6297c35d2fa313f66605e3e379db08eff6ce89bf
c-lobato <caiolpbarreto@gmail.com>
2026-05-26 17:55:35 -0300

    .

--- shoot'em-up-/scripts/player/Player.cs
+++ shoot'em-up-/scripts/player/Player.cs
@@ -1,6 +1,12 @@
 using Godot;
 using System;
 
-public partial class Player : Node
+public partial class Player : CharacterBody2D
 {
+    [Export] protected int Speed = 400;
+    [Export] protected int Health = 5;
+    
+    protected int Score;
+     
+    
 }
